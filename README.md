1dd202b05b407789c2a1985bec8988a92c60583a
d3fault <d3fault@d3fault.net>
2025-02-20 16:27:11 -0700

    add fly-around-bullet-physics-bgfx-wasm-template-starting-point

---
+++ README.md
@@ -0,0 +1,1 @@
+# [WebAssembly (wasm) 3D Demos](http://d3fault.github.io/wasm-3d-demos/index.html)
---
+++ fly-around-bullet-physics-bgfx-wasm-template-starting-point/CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/main.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/main.cpp.o.d"
+  "fly-around-bullet-physics-bgfx-wasm-template-starting-point.js"
+  "fly-around-bullet-physics-bgfx-wasm-template-starting-point.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

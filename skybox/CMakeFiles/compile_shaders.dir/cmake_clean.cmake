552e4d224c21e799498441b826edd620d20cd1bb
d3fault <d3fault@d3fault.net>
2025-02-27 14:09:25 -0700

    add skybox

---
+++ skybox/CMakeFiles/compile_shaders.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compile_shaders"
+  "fs_overlay.bin"
+  "fs_skybox.bin"
+  "vs_overlay.bin"
+  "vs_skybox.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/compile_shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ skybox/CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/bgfx2doverlaytext_dynamicallysized.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/bgfx2doverlaytext_dynamicallysized.cpp.o.d"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/bgfx2doverlaytext_staticallysized.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/bgfx2doverlaytext_staticallysized.cpp.o.d"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/lolreadfile.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/lolreadfile.cpp.o.d"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/main.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/main.cpp.o.d"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/skybox.cpp.o"
+  "CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/skybox.cpp.o.d"
+  "fly-around-bullet-physics-bgfx-wasm-template-starting-point.js"
+  "fly-around-bullet-physics-bgfx-wasm-template-starting-point.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fly-around-bullet-physics-bgfx-wasm-template-starting-point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

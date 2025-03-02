24d1404120d0b0c6ceff1519c4a33c89b2a7a87f
d3fault <d3fault@d3fault.net>
2025-03-02 17:40:47 -0500

    add bgfx-assimp-3d-pbr-ibl-shiny-drill

---
+++ bgfx-assimp-3d-pbr-ibl-shiny-drill/CMakeFiles/drill.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drill.dir/main.cpp.o"
+  "CMakeFiles/drill.dir/main.cpp.o.d"
+  "drill.js"
+  "drill.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bgfx-assimp-3d-pbr-ibl-shiny-drill/CMakeFiles/drill.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for drill.
+# This may be replaced when dependencies are built.
